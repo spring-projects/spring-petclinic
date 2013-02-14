7a5af7a13b39f5c73919a6eaf74fc67957771aed
Mic <misvy@vmware.com>
2013-02-14 11:56:45 +0800

    improved Javadoc comments and added licence info

--- src/main/java/org/springframework/samples/petclinic/util/CallMonitoringAspect.java
+++ src/main/java/org/springframework/samples/petclinic/util/CallMonitoringAspect.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.samples.petclinic.util;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -9,8 +24,8 @@ import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.util.StopWatch;
 
 /**
- * Simple AspectJ aspect that monitors call count and call invocation time.
- * Implements the CallMonitor management interface.
+ * Simple aspect that monitors call count and call invocation time.
+ * It uses JMX annotations and therefore can be monitored using any JMX console such as the jConsole
  *
  * @author Rob Harrop
  * @author Juergen Hoeller
