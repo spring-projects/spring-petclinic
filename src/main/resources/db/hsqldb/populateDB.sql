9ee5e6f096e67112f070cbd99c5ba431b0bb22c3
Mic <misvy@vmware.com>
2013-02-21 14:50:00 +0800

    visit dates were not consistent

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -47,7 +47,7 @@ INSERT INTO pets VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets VALUES (13, 'Sly', '2012-06-08', 1, 10);
 
-INSERT INTO visits VALUES (1, 7, '1996-03-04', 'rabies shot');
-INSERT INTO visits VALUES (2, 8, '1996-03-04', 'rabies shot');
-INSERT INTO visits VALUES (3, 8, '1996-06-04', 'neutered');
-INSERT INTO visits VALUES (4, 7, '1996-09-04', 'spayed');
+INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
+INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
+INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
+INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
