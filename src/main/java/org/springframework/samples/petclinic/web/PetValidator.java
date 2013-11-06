34d8ca46ac9add81362532e41461ceb52aa67fbc
Mic <misvy@vmware.com>
2013-11-06 09:18:33 +0800

    chaining validation so we can see multiple error messages when there are multiple validation errors

--- src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetValidator.java
@@ -29,12 +29,21 @@ public class PetValidator {
 
     public void validate(Pet pet, Errors errors) {
         String name = pet.getName();
+        // name validaation
         if (!StringUtils.hasLength(name)) {
             errors.rejectValue("name", "required", "required");
         } else if (pet.isNew() && pet.getOwner().getPet(name, true) != null) {
             errors.rejectValue("name", "duplicate", "already exists");
-        } else if (pet.isNew() && pet.getType() == null) {
+        }
+        
+        // type valication
+        if (pet.isNew() && pet.getType() == null) {
             errors.rejectValue("type", "required", "required");
+        }
+        
+     // type valication
+        if (pet.getBirthDate()==null) {
+            errors.rejectValue("birthDate", "required", "required");
         }
     }
 
