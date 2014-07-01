521155c3a64f50609767616213bf7d37906da954
Antoine Rey <antoine.rey@gmail.com>
2014-07-01 08:00:09 +0200

    Setup Java configuration into the web.xml

---
+++ src/main/java/org/springframework/samples/petclinic/config/MvcCoreConfig.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:spring/mvc-core-config.xml")
+public class MvcCoreConfig {
+
+}
