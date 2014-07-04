f92c31c31a1570bf8fc6d8f91ccafbde4cf3cd6e
Antoine Rey <antoine.rey@gmail.com>
2014-07-04 08:17:23 +0200

    Display at startup Spring active profiles

--- src/main/java/org/springframework/samples/petclinic/config/RootApplicationContextConfig.java
+++ src/main/java/org/springframework/samples/petclinic/config/RootApplicationContextConfig.java
@@ -1,10 +1,41 @@
 package org.springframework.samples.petclinic.config;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @Import({BusinessConfig.class, ToolsConfig.class})
 public class RootApplicationContextConfig {
+	
+    private static final Logger LOG = LoggerFactory.getLogger(RootApplicationContextConfig.class);
+
+    @Autowired
+    private Environment         env;
+
+    /**
+     * Application custom initialization code.
+     * <p/>
+     * Spring profiles can be configured with a system property
+     * -Dspring.profiles.active=javaee
+     * <p/>
+     */
+    @PostConstruct
+    public void initApp() {
+        LOG.debug("Looking for Spring profiles...");
+        if (env.getActiveProfiles().length == 0) {
+            LOG.info("No Spring profile configured, running with default configuration.");
+        } else {
+            for (String profile : env.getActiveProfiles()) {
+                LOG.info("Detected Spring profile: {}", profile);
+            }
+        }
+    }
+
 
 }
