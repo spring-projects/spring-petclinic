0db296c32d293eca2cfddc7012936bd863007d6f
Nikolay Yurin <nyurin@griddynamics.com>
2014-08-14 14:19:41 +0400

    [DMG-365] Add asf-webapp-demo deployment
    - Add asf-webapp-demo cookbook
    - Add Qubell manifest

---
+++ cookbooks/asf-webapp-demo/Berksfile
@@ -0,0 +1,3 @@
+source "https://supermarket.getchef.com"
+
+metadata
---
+++ cookbooks/asf-webapp-demo/metadata.rb
@@ -0,0 +1,10 @@
+# Encoding: utf-8
+name             'asf-webapp-demo'
+maintainer       'Copyright (c) 2014 Grid Dynamics International, Inc. All Rights Reserved'
+maintainer_email 'nyurin@griddynamics.com'
+license          'Apache 2.0'
+description      'Installs/Configures asf-webapp-demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum-epel'
