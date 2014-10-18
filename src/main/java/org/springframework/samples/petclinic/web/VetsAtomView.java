475f5f534910aa2bff5563eab9c4eec486430e48
michaelisvy <misvy@gopivotal,com>
2014-10-18 23:36:15 +0800

    migrated to Spring 4.1.1.RELEASE
    Also had to make changes because Spring now uses com.rometools for RSS
    feeds
    See here for more details: https://jira.spring.io/browse/SPR-11893

--- src/main/java/org/springframework/samples/petclinic/web/VetsAtomView.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetsAtomView.java
@@ -15,20 +15,19 @@
  */
 package org.springframework.samples.petclinic.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.rometools.rome.feed.atom.Entry;
+import com.rometools.rome.feed.atom.Feed;
+import com.rometools.rome.feed.atom.Content;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.web.servlet.view.feed.AbstractAtomFeedView;
 
-import com.sun.syndication.feed.atom.Content;
-import com.sun.syndication.feed.atom.Entry;
-import com.sun.syndication.feed.atom.Feed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * A view creating a Atom representation from a list of Visit objects.
