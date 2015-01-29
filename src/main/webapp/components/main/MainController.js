2cbd654b77c5c982ab89328d9572bbdb554e69c0
Nguyen Anh Tuan, Tony <tuanngda@gmail.com>
2015-01-29 08:17:15 +0800

    TN: Make main page scroll down to Vets when clicking on the link

--- src/main/webapp/components/main/MainController.js
+++ src/main/webapp/components/main/MainController.js
@@ -4,6 +4,12 @@ var MainController = function($scope, $rootScope, $sce, $timeout, $location,
   $scope.$route = $route;
   $scope.$location = $location;
   $scope.footerText = '© ' + new Date().getFullYear() + ' Pet Clinic, A Spring Framework Demonstration';
+  
+  $scope.scrollToVet = function(){
+		$('html, body').animate({
+			scrollTop : $("#veterianarians").offset().top
+		}, 1000);
+  }
 };
 
 var MainControllerDeclaration = [ '$scope', '$rootScope', '$sce', '$timeout','$location', '$route', '$interval', '$cookieStore', '$window', MainController ];
