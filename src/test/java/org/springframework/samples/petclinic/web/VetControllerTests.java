98d9bbb6296300db5d0a98edb8bfdf4bf95b02c0
Antoine Rey <antoine.rey@free.fr>
2015-06-24 18:43:45 +0200

    Removing reference to the unknown UserResource class

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
@@ -21,8 +21,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Test class for the UserResource REST controller.
- *
- * @see UserResource
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/business-config.xml", "classpath:spring/tools-config.xml", "classpath:spring/mvc-core-config.xml"})
