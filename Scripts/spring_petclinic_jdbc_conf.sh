fd1074da989cd6aa1b764116bcd2a82147dd880c
sada <sada@sd.hpsw.local>
2015-10-07 16:22:03 +0900

    mysql対応 & shell script 追加

---
+++ Scripts/spring_petclinic_jdbc_conf.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# PetClinic appliaction deployment script
+TMPDIR=/tmp/arm-workdir
+PROPFILE=$TMPDIR/petclinic/WEB-INF/classes/spring/data-access.properties
+
+mkdir -p $TMPDIR/petclinic
+unzip /tmp/petclinic.war -d $TMPDIR/petclinic
+
+
+sed -e "s/\${HOSTNAME}/$1/g" \
+    -e "s/\${SERNAME}/$2/g" \
+    -e "s/\${PASSWORD}/$3/g" $PROPFILE > /tmp/jdbc.tmp$$
+
+
+cp -f /tmp/jdbc.tmp$$ $PROPFILE
+rm -f /tmp/jdbc.tmp$$
+
+cd /tmp/arm-workdir/petclinic
+zip -r /tmp/arm-workdir/petclinic-updated.zip ./*
+mv /tmp/arm-workdir/petclinic-updated.zip /tmp/arm-workdir/petclinic.war
+sudo mv /tmp/arm-workdir/petclinic.war $4/webapps
+
+#sudo  sh /opt/tomcat7/bin/shutdown.sh
+#sudo  sh /opt/tomcat7/bin/startup.sh
+exit 0
