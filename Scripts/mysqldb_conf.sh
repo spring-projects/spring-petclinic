9e1618e1dfd181fa791312738a014eac834cffc6
sada <sada@sd.hpsw.local>
2015-10-09 14:56:26 +0900

    enhance shell script

---
+++ Scripts/mysqldb_conf.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+### PetClinic Db creation script
+
+MyUSER="$1"
+MyPASS="$2"
+
+#sudo mysqladmin -u root password $MyPASS
+
+C1="CREATE DATABASE IF NOT EXISTS petclinic;"
+C2="USE petclinic;"
+C3="GRANT ALL PRIVILEGES ON *.*  TO '$MyUSER'@'%' IDENTIFIED BY '$MyPASS' WITH GRANT OPTION;"
+C4="FLUSH PRIVILEGES;"
+SQL="${C1}${C2}${C3}${C4}"
+
+#sudo MYSQL_PWD="$MyPASS" mysql -h localhost "--user=root" -Bse "$SQL"
+sudo mysql -Bse "$SQL"
+if [ $? != 0 ];then
+	echo "ERROR: sudo mysql -Bse \"$SQL\""
+	exit 1
+fi
+
+
+exit 0
--- Scripts/spring_petclinic_jdbc_conf.sh
+++ Scripts/spring_petclinic_jdbc_conf.sh
@@ -1,27 +1,56 @@
 #!/bin/sh
 
+
 # PetClinic appliaction deployment script
-TMPDIR=/tmp/arm-workdir
+TMPDIR=$TMPDIR
 PROPFILE=$TMPDIR/petclinic/WEB-INF/classes/spring/data-access.properties
 
 mkdir -p $TMPDIR/petclinic
-unzip /tmp/petclinic.war -d $TMPDIR/petclinic
+if [ $? != 0 ];then
+	echo "ERROR: mkdir -p $TMPDIR/petclinic"
+	exit 1
+fi 
 
+unzip /tmp/petclinic.war -d $TMPDIR/petclinic
+if [ $? != 0 ];then
+	echo "ERROR: unzip /tmp/petclinic.war -d $TMPDIR/petclinic"
+	exit 1
+fi 
 
 sed -e "/# HSQL/,/# MySQL/ s/^/#/g" \
     -e "/# MySQL/,$ s/^#\([jh]\)/\1/g" \
     -e "s/\${HOSTNAME}/$1/g" \
     -e "s/\${USERNAME}/$2/g" \
     -e "s/\${PASSWORD}/$3/g" $PROPFILE > /tmp/jdbc.tmp$$
+if [ $? != 0 ];then
+	echo "ERROR: sed $PROFILE > /tmp/jdbc.tmp$$"
+	exit 1
+fi 
 
 
-cp -f /tmp/jdbc.tmp$$ $PROPFILE
-rm -f /tmp/jdbc.tmp$$
+mv -f /tmp/jdbc.tmp$$ $PROPFILE
+if [ $? != 0 ];then
+	echo "ERROR: mv -f /tmp/jdbc.tmp$$ $PROPFILE"
+	exit 1
+fi 
 
-cd /tmp/arm-workdir/petclinic
-zip -r /tmp/arm-workdir/petclinic-updated.zip ./*
-mv /tmp/arm-workdir/petclinic-updated.zip /tmp/arm-workdir/petclinic.war
-sudo mv /tmp/arm-workdir/petclinic.war $4/webapps
+cd $TMPDIR/petclinic
+zip -r $TMPDIR/petclinic-updated.zip ./*
+if [ $? != 0 ];then
+	echo "ERROR: zip -r $TMPDIR/petclinic-updated.zip"
+	exit 1
+fi
+
+mv $TMPDIR/petclinic-updated.zip $TMPDIR/petclinic.war
+if [ $? != 0 ];then
+	echo "ERROR: mv $TMPDIR/petclinic-updated.zip $TMPDIR/petclinic.war"
+	exit 1
+fi
+sudo mv $TMPDIR/petclinic.war $4/webapps
+if [ $? != 0 ];then
+	echo "ERROR: mv $TMPDIR/petclinic.war $4/webapps"
+	exit 1
+fi
 
 #sudo  sh /opt/tomcat7/bin/shutdown.sh
 #sudo  sh /opt/tomcat7/bin/startup.sh
