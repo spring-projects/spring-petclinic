e7f68999bfa947cfb39601502da8c15e83285196
kadinyazilimci <armagan.ersoz@gmail.com>
2016-02-25 09:36:20 +1000

    owners search has been case insensitive

--- src/main/resources/db/hsqldb/initDB.sql
+++ src/main/resources/db/hsqldb/initDB.sql
@@ -36,7 +36,7 @@ CREATE INDEX types_name ON types (name);
 CREATE TABLE owners (
   id         INTEGER IDENTITY PRIMARY KEY,
   first_name VARCHAR(30),
-  last_name  VARCHAR(30),
+  last_name  VARCHAR_IGNORECASE(30),
   address    VARCHAR(255),
   city       VARCHAR(80),
   telephone  VARCHAR(20)
