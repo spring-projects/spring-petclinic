f8efcf5bdd0574c6115c6b66bf31cc6b84dc37dd
mitesh51 <mitesh.soni83@gmail.com>
2016-09-18 14:38:16 +0530

    Changed import

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -15,7 +15,7 @@
  */
 package org.springframework.samples.petclinic.model;
 
-impor java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
