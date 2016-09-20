8d1efb240b16b5985f1c155a405c8484529ce1a0
Dapeng <verydapeng@gmail.com>
2016-09-20 18:11:25 +0800

    optimize static resources

--- src/test/java/org/springframework/samples/petclinic/web/PetResourceTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/PetResourceTests.java
@@ -1,53 +1,64 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
-
-/**
- * Test class for the UserResource REST controller.
- *
- * @see UserResource
- */
-public class PetResourceTests extends AbstractWebResourceTests {
+@RunWith(SpringRunner.class)
+@WebMvcTest(PetResource.class)
+public class PetResourceTests {
 
     @Autowired
-    private PetResource petResource;
-    
-    @Before
-    public void setup() {
-    	runMockSpringMVC(petResource);
-    }
+    private MockMvc mvc;
 
-    /**
-     * Expected JSon result:
-     * {  
-		   "id":2,
-		   "name":"Basil",
-		   "birthDate":1344211200000,
-		   "type":{  
-		      "id":6,
-		      "name":"hamster",
-		      "new":false
-		   },
-		   "visits":[],
-		   "new":false
-		}
-     */
+    @MockBean
+    ClinicService clinicService;
+
     @Test
     public void shouldGetAPetInJSonFormat() throws Exception {
-    	ResultActions actions = mockMvc.perform(get("/owner/2/pet/2.json").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    	actions.andExpect(content().contentType("application/json;charset=UTF-8"))
+
+        Pet pet = setupPet();
+
+        given(clinicService.findPetById(2)).willReturn(pet);
+
+
+        mvc.perform(get("/owner/2/pet/2.json").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$.id").value(2))
                 .andExpect(jsonPath("$.name").value("Basil"))
-                .andExpect(jsonPath("$.type.id").value(6)); 
+                .andExpect(jsonPath("$.type.id").value(6));
+    }
+
+    private Pet setupPet() {Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+
+        Pet pet = new Pet();
+
+        pet.setName("Basil");
+        pet.setId(2);
+
+        PetType petType = new PetType();
+        petType.setId(6);
+        pet.setType(petType);
+
+        owner.addPet(pet);
+        return pet;
     }
 }
