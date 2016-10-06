f78ee5d76e13b535e55298704cd090c60515cb38
Antoine Rey <antoine.rey@gmail.com>
2016-10-06 20:01:05 +0200

    Using the @CacheResult JSR-107 JCache annotation

--- src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -15,16 +15,9 @@
  */
 package org.springframework.samples.petclinic.service;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.model.Visit;
+import org.springframework.samples.petclinic.model.*;
 import org.springframework.samples.petclinic.repository.OwnerRepository;
 import org.springframework.samples.petclinic.repository.PetRepository;
 import org.springframework.samples.petclinic.repository.VetRepository;
@@ -32,9 +25,12 @@ import org.springframework.samples.petclinic.repository.VisitRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.cache.annotation.CacheResult;
+import java.util.Collection;
+
 /**
  * Mostly used as a facade for all Petclinic controllers
- * Also a placeholder for @Transactional and @Cacheable annotations
+ * Also a placeholder for @Transactional and @CacheResult annotations
  *
  * @author Michael Isvy
  */
@@ -100,7 +96,7 @@ public class ClinicServiceImpl implements ClinicService {
 
     @Override
     @Transactional(readOnly = true)
-    @Cacheable(value = "vets")
+    @CacheResult(cacheName = "vets")
     public Collection<Vet> findVets() throws DataAccessException {
         return vetRepository.findAll();
     }
