dd1df85e603668aa14091eb914929d982b75002f
Antoine Rey <antoine.rey@gmail.com>
2016-10-25 19:02:31 +0200

    Adding typesafe custom configuration

--- springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -2,7 +2,9 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.samples.petclinic.config.PetclinicProperties;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PetclinicProperties.class)
 public class PetClinicApplication {
 
     public static void main(String[] args) {
