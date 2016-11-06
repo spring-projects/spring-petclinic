93738227b4414c22b43f9ef7ac98cc2aea356209
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2016-11-06 11:21:23 +0200

    change setOwner() method scope to public

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -89,7 +89,7 @@ public class Pet extends NamedEntity {
         return this.owner;
     }
 
-    protected void setOwner(Owner owner) {
+    public void setOwner(Owner owner) {
         this.owner = owner;
     }
 
