75912a06c5613a2ea1305ad4d8ad6bc4be7765ce
Stephane Nicoll <snicoll@pivotal.io>
2017-02-17 12:30:57 +0100

    Polish contribution
    Closes gh-229

--- src/main/java/org/springframework/samples/petclinic/system/CacheConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfig.java
@@ -3,9 +3,9 @@ package org.springframework.samples.petclinic.system;
 import javax.cache.configuration.Configuration;
 import javax.cache.configuration.MutableConfiguration;
 
-import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 /**
@@ -14,17 +14,19 @@ import org.springframework.context.annotation.Profile;
 @org.springframework.context.annotation.Configuration
 @EnableCaching
 @Profile("production")
-class CacheConfig implements CacheManagerCustomizer<JCacheCacheManager> {
+class CacheConfig {
 
-    @Override
-    public void customize(JCacheCacheManager cacheManager) {
-        Configuration<Object, Object> cacheConfiguration = createCacheConfiguration();
-        cacheManager.getCacheManager().createCache("vets", cacheConfiguration);
+    @Bean
+    public JCacheManagerCustomizer cacheManagerCustomizer() {
+        return cm -> {
+            Configuration<Object, Object> cacheConfiguration = createCacheConfiguration();
+            cm.createCache("vets", cacheConfiguration);
+        };
     }
 
     private Configuration<Object, Object> createCacheConfiguration() {
-        // Create a cache using infinite heap. A real application will want to use an implementation dependent
-        // configuration that will better fit your needs
+        // Create a cache using infinite heap. A real application will want to use an
+        // implementation dependent configuration that will better fit your needs
         return new MutableConfiguration<>().setStatisticsEnabled(true);
     }
 }
