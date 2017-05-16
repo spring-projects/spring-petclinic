ed6da90d64001eef2866aa5f4d6ed8a30962283f
Dmitry <dmitry@liatrio.com>
2017-05-15 17:41:56 -0700

    fixed formatting

--- petclinic_spec.rb
+++ petclinic_spec.rb
@@ -18,35 +18,35 @@ describe 'Petlinic' do
 
   describe 'when homepage is available' do
     it 'should show correct page title' do
-        sleep 3
-        assert @driver.title == 'PetClinic :: a Spring Framework demonstration'
+      sleep 3
+      assert @driver.title == 'PetClinic :: a Spring Framework demonstration'
     end
   end
 
   describe 'when homepage is available' do
     it 'should have Find Owners page' do
-        @driver.find_element(:class, 'icon-search').click
-        sleep 3
-        h2 = @driver.find_element(:tag_name, 'h2')
-        assert h2.text == 'Find Owners'
+      @driver.find_element(:class, 'icon-search').click
+      sleep 3
+      h2 = @driver.find_element(:tag_name, 'h2')
+      assert h2.text == 'Find Owners'
     end
   end
 
   describe 'when homepage is available' do
-      it 'should have veterinarians page' do
-          @driver.find_element(:class, 'icon-th-list').click
-          sleep 3
-          h2 = @driver.find_element(:tag_name, 'h2')
-          assert h2.text == 'Veterinarians'
-      end
+    it 'should have veterinarians page' do
+      @driver.find_element(:class, 'icon-th-list').click
+      sleep 3
+      h2 = @driver.find_element(:tag_name, 'h2')
+      assert h2.text == 'Veterinarians'
+    end
   end
 
   describe 'when homepage is available' do
-      it 'should have search for veterinarian feature' do
-          @driver.find_element(:class, 'icon-th-list').click
-          @driver.find_element(:tag_name, 'input').send_keys('Helen Leary')
-          result = @driver.find_element(:css, 'td.sorting_1').text
-          assert result == 'Helen Leary'
-      end
+    it 'should have search for veterinarian feature' do
+      @driver.find_element(:class, 'icon-th-list').click
+      @driver.find_element(:tag_name, 'input').send_keys('Helen Leary')
+      result = @driver.find_element(:css, 'td.sorting_1').text
+      assert result == 'Helen Leary'
+    end
   end
 end
