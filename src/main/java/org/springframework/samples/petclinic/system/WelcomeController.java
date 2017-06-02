1911f805e6264629101145813597d9efa2b0ebce
Art O Cathain <art.ocathain@bjss.com>
2017-06-02 11:27:11 +0100

    Create sample data programattically

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,14 +1,27 @@
 package org.springframework.samples.petclinic.system;
 
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 class WelcomeController {
 
-    @RequestMapping("/")
+    @Autowired
+    private SampleData sampleData;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
     public String welcome() {
         return "welcome";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/")
+    public String reset() {
+        LoggerFactory.getLogger(getClass()).info("Resetting");
+        sampleData.reset();
+        return "redirect:/";
     }
 }
