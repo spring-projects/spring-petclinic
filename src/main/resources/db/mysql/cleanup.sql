c818b982d5989e0f6f67212a065014134044df09
Murray Brown <mbrown@splicemachine.com>
2017-10-23 18:02:47 -0700

    A script to drop all the tables in reverse order

---
+++ src/main/resources/db/mysql/cleanup.sql
@@ -0,0 +1,7 @@
+DROP TABLE if EXISTS visits;
+DROP TABLE if EXISTS pets;
+DROP TABLE if EXISTS owners;
+DROP TABLE if EXISTS types;
+DROP TABLE if EXISTS vet_specialties;
+DROP TABLE if EXISTS specialties;
+DROP TABLE if EXISTS vets;
