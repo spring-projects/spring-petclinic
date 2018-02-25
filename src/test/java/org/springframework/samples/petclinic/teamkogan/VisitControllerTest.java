965725479160f5c64a6e689aed51e16ea90859f1
EulPi <jeff.casimir@gmail.com>
2018-02-24 19:53:28 -0500

    Fixed indentation from copy/paste

--- src/test/java/org/springframework/samples/petclinic/teamkogan/VisitControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/teamkogan/VisitControllerTest.java
@@ -17,22 +17,22 @@ public class VisitControllerTest
     @Test
     public void testPetVisitDate()
     {
-	// Define random pet ID
-	final Integer PET_ID = 7;
-	Pet pet7 = new Pet();
-	
-	// Mock dependencies
-	VisitRepository mockVisitRepo = mock(VisitRepository.class);
-	PetRepository mockPetRepo = mock(PetRepository.class);
-	VisitController visitController = new VisitController(mockVisitRepo, mockPetRepo);
-	
-	// Define mock behaviour	
-	when(mockPetRepo.findById(PET_ID)).thenReturn(pet7);
-	
-	// Call method under inspection
-	Visit visit = visitController.loadPetWithVisit(PET_ID, new HashMap<>());
-	
-	// Confirm that the same visit date was assigned to the pet
-	assertEquals(visit.getDate(), pet7.getVisits().get(0).getDate());
+		// Define random pet ID
+		final Integer PET_ID = 7;
+		Pet pet7 = new Pet();
+		
+		// Mock dependencies
+		VisitRepository mockVisitRepo = mock(VisitRepository.class);
+		PetRepository mockPetRepo = mock(PetRepository.class);
+		VisitController visitController = new VisitController(mockVisitRepo, mockPetRepo);
+		
+		// Define mock behaviour	
+		when(mockPetRepo.findById(PET_ID)).thenReturn(pet7);
+		
+		// Call method under inspection
+		Visit visit = visitController.loadPetWithVisit(PET_ID, new HashMap<>());
+		
+		// Confirm that the same visit date was assigned to the pet
+		assertEquals(visit.getDate(), pet7.getVisits().get(0).getDate());
     }
 }
