8f516f8e3d7ca53e3abce25eabdda32e478ad4d7
Amanda <amawai@github.com>
2018-02-24 23:39:23 -0500

    Added comments for clarity

--- src/test/java/org/springframework/samples/petclinic/visit/VisitRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/visit/VisitRepositoryTest.java
@@ -3,14 +3,13 @@ package org.springframework.samples.petclinic.visit;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -38,14 +37,14 @@ public class VisitRepositoryTest {
 	@Test
 	public void shouldFindSavedVisitInVisitRepository() {
 		visitRepository.save(visit);
+		//Get the list of visits associated to VISIT_ID
 		visitList = visitRepository.findByPetId(VISIT_ID);
-		
-		assertEquals(visitList.size(), 1);
-		Visit savedVisit = visitList.get(0);
-		assertEquals((int) savedVisit.getPetId(), VISIT_ID);
-		assertEquals(savedVisit.getDescription(), DESCRIPTION);
-		assertEquals(savedVisit.getDate(), TODAY);
-	}
-	
 
+		assertThat(visitList.size()).isEqualTo(1);
+		Visit savedVisit = visitList.get(0);
+
+		assertThat((int) savedVisit.getPetId()).isEqualTo(VISIT_ID);
+		assertThat(savedVisit.getDescription()).isEqualTo(DESCRIPTION);
+		assertThat(savedVisit.getDate()).isEqualTo(TODAY);
+	}
 }
