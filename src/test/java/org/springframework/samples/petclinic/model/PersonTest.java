d2637471beb459cf4af39406da120ca6fa87a5c9
Aman Bhandal <aman_bhandal@live.ca>
2018-02-25 13:44:32 -0500

    [TEST] Added tests for Person, BaseEntity, and NamedEntity

---
+++ src/test/java/org/springframework/samples/petclinic/model/PersonTest.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    private Person personObj;
+
+    @Before
+    public void setUp(){
+        personObj = new Person();
+    }
+
+    // Testing the DAO of Person class by verifying the setter and getter of FirstName
+    @Test
+    public void testSetAndGetFirstName() {
+        personObj.setFirstName("Johnny");
+    }
+
+    // Testing the DAO of Person class by verifying the setter and getter of FirstName
+    @Test
+    public void testSetAndGetLastName() {
+        personObj.setLastName("Oliver");
+        assertEquals("Oliver", personObj.getLastName());
+    }
+}
