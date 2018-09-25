583f101a0c7a1005c2b3da21b89d997fb02aaa37
Alexander Sharov <kvendingoldo@yandex.ru>
2018-09-25 20:37:21 +0400

    Reorganize project

---
+++ udc-backend/src/main/java/org/springframework/samples/petclinic/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The classes in this package represent utilities used by the domain.
+ */
+package main.java.org.springframework.samples.petclinic.model;
+
---
+++ udc-backend/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -0,0 +1,14 @@
+package main.java.org.springframework.samples.petclinic.system;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+}
