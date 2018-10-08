c769cddd732357707130d2117297818861775d25
Juan Antonio Brena Moral <bren@juanantonio.info>
2018-10-08 21:51:29 +0200

    Removing * in import. Following Spring coding convention.

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -15,7 +15,13 @@
  */
 package org.springframework.samples.petclinic.vet;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
