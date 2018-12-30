c19974af0f04c61c1ffc55006967db09ae4bfced
Andrii Skrypnychenko <scrip.a.v@gmail.com>
2018-12-30 01:19:25 +0100

    Added new Pet type: dragon

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -21,6 +21,7 @@ INSERT IGNORE INTO types VALUES (3, 'lizard');
 INSERT IGNORE INTO types VALUES (4, 'snake');
 INSERT IGNORE INTO types VALUES (5, 'bird');
 INSERT IGNORE INTO types VALUES (6, 'hamster');
+INSERT IGNORE INTO types VALUES (7, 'dragon');
 
 INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
 INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
