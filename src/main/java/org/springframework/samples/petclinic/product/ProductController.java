3aac648f80724c0b06659fac7c89a6ecdbb72529
cgsrjunior <Passei@#$234>
2019-08-09 15:33:26 -0300

    #2                                                         Controlador para produtos básico. Exibe um Hello na tela

---
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public String showProductList(){
+        return "Hello!";
+    }
+}
