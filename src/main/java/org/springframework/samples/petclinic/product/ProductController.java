8b3d8f8e8f7a3c56c212504e870fbbee05bbcd69
ipagnoncelli <isabella.pagnoncelli@hotmail.com>
2019-08-09 17:10:14 -0300

    .

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -3,10 +3,19 @@ package org.springframework.samples.petclinic.product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 public class ProductController {
     @GetMapping("/products")
-    public String showProductList(){
-        return "Hello!";
+    public String showProductList(Model model){
+        List<Product> prods = new ArrayList<Product>();
+        model.addAttribute("product",prods);
+        //prods.add(new Product("p1");
+        //prods.add(new Product("P2"));
+        return "products/productsList";
     }
-}
+
+    }
+
