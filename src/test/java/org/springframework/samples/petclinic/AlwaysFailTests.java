405b3566675034b6af80a5762b7fb5372335c8d2
Artem Dobrynin <Artem_Dobrynin@epam.com>
2019-08-14 10:25:43 +0400

    Jenkins & SQ Test Commit
    - removed failed test in case of jenkins job testing

--- src/test/java/org/springframework/samples/petclinic/AlwaysFailTests.java
+++ src/test/java/org/springframework/samples/petclinic/AlwaysFailTests.java
@@ -3,17 +3,12 @@ package org.springframework.samples.petclinic;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
 public class AlwaysFailTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    public void failTest() throws Exception{
-        assertEquals(1, 2);
+    public void failTest() throws Exception {
+        assertEquals(2, 2);
     }
 
 }
