3d6c9040b271697139ccf8ead6cf623c6552012d
Andrew Pitt <apitt@redhat.com>
2020-01-06 15:00:34 -0500

    Updated test

--- src/test/java/integration/MainPageIT.java
+++ src/test/java/integration/MainPageIT.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 
 import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MainPageIT {
@@ -31,9 +32,9 @@ public class MainPageIT {
 		WebDriver driver = config.getDriver();
 		driver.get(url);
 		assertEquals("Title not as expected: ", "PetClinic :: a Spring Framework demonstration", getTitle());
-//		String welcomeText = driver.findElement(By.id("welcome")).getText();
-//		assertEquals("Wrong welcome text.", "Welcome to the Pet Clinic!", welcomeText);
-//		System.out.println("Text extracted: " + welcomeText);
+		String welcomeText = driver.findElement(By.id("welcome")).getText();
+		assertEquals("Wrong welcome text.", "Welcome to the Red Hat Pet Clinic!", welcomeText);
+		System.err.println("Text extracted: " + welcomeText);
 	}
 
 }
