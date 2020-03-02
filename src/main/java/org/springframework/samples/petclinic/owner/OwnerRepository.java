b9414e29d707cc99ad9007b0261bad3013a1d7fb
Hoyoung Jung <honux77@gmail.com>
2020-03-02 18:26:59 +0900

    Feat: Search by firstName
    인프런 기선님 강의 과제 1

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -61,4 +61,7 @@ public interface OwnerRepository extends Repository<Owner, Integer> {
 	 */
 	void save(Owner owner);
 
+    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.firstName LIKE :firstName%")
+    @Transactional(readOnly = true)
+    Collection<Owner> findByFirstName(String firstName);
 }
