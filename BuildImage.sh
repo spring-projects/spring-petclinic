96761db6e9842c04e3f7c968fa3facf57548d054
ametgud4u <64977602+ametgud4u@users.noreply.github.com>
2020-07-26 22:35:26 +0530

    Create BuildImage.sh

---
+++ BuildImage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo rm -rf /home/jenkins/centos_data/apps/java-app/
+git clone https://github.com/ametgud4u/spring-petclinic.git /home/jenkins/centos_data/apps/java-app/
+
+echo "*****************************************"
+echo "*****************Build*******************"
+docker run --rm -ti -v $PWD/java-app:/apps -v /root/.m2/:/root/.m2/ -w /apps/ maven3:alphine "$@"
