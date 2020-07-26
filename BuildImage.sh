441de0a8695f0eb2001c1667d87f1b66bf770956
ametgud4u <64977602+ametgud4u@users.noreply.github.com>
2020-07-26 23:50:28 +0530

    Update BuildImage.sh

--- BuildImage.sh
+++ BuildImage.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# copying  runtime to docker build path
-sudo cp -f /home/jenkins/centos_data/apps/workspace/SpringPetClinic-Piple/target/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar /home/jenkins/centos_data/Build/
-
 echo " *************************************** "
 echo " ********** Build Docker Image********** "
 echo " *************************************** "
 
-cd /home/jenkins/centos_data/Build/ && docker-compose -f docker-compose-buildimage.yml  build --no-cache
+docker-compose -f docker-compose-buildimage.yml  build --no-cache
