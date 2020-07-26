ae1dd5d669d3d991ff3be1f2559a85c6cccd64ed
ametgud4u <64977602+ametgud4u@users.noreply.github.com>
2020-07-27 00:04:20 +0530

    Update BuildImage.sh

--- BuildImage.sh
+++ BuildImage.sh
@@ -4,4 +4,4 @@ echo " *************************************** "
 echo " ********** Build Docker Image********** "
 echo " *************************************** "
 
-docker-compose -f docker-compose-buildimage.yml  build --no-cache
+sudo docker-compose -f docker-compose-buildimage.yml  build --no-cache
