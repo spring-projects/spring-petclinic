e7ab071340b59cdc6ee61a577e54a5cdb596ac4c
Cesarion Pshebytski <yur6996@ukr.net>
2020-08-19 23:00:24 +0300

    Updated user for mysql
    Updated creation of user for mysql db. Previously lack of quotes resulted in creation of incorrect user (petclinic@% instead of just petclinic)

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -4,4 +4,4 @@ ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic@%' IDENTIFIED BY 'petclinic';
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
