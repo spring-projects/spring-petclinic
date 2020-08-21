ee7c68ff3bd516a2e8ad9ff1d3c6cbcf65c22454
Abhijay Kumar <abhijay.kumar@centricconsulting.com>
2020-08-21 19:36:11 +0530

    Fixed code smell for single-line lambda expression
    Lambdas containing only one statement should not nest this statement in a block
    RSPEC-1602(https://rules.sonarsource.com/java/RSPEC-1602)

--- src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
@@ -34,9 +34,7 @@ class CacheConfiguration {
 
 	@Bean
 	public JCacheManagerCustomizer petclinicCacheConfigurationCustomizer() {
-		return cm -> {
-			cm.createCache("vets", cacheConfiguration());
-		};
+		return cm -> cm.createCache("vets", cacheConfiguration());
 	}
 
 	/**
