e4e044782edaf9899af4e4aebc82836544133cda
Marc <first.foward.sprl@gmail.com>
2020-11-17 16:15:23 +0100

    start with a blank page and run spring boot

---
+++ src/main/java/guru/springframework/springpetclinic/SpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/springpetclinic/SpringPetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
