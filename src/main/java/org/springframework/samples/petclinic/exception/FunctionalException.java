f5c0430bbb6615d69f91fbe61c02241773225ae5
PEDSF <pe.dsf@free.fr>
2020-12-16 10:51:13 +0100

    change travis java 9 to 11

--- src/main/java/org/springframework/samples/petclinic/exception/FunctionalException.java
+++ src/main/java/org/springframework/samples/petclinic/exception/FunctionalException.java
@@ -5,36 +5,33 @@ package org.springframework.samples.petclinic.exception;
  */
 public class FunctionalException extends Exception {
 
-    /** serialVersionUID */
-    private static final long serialVersionUID = 1L;
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1L;
 
+	// ==================== Constructeurs ====================
+	/**
+	 * Constructeur.
+	 * @param pMessage -
+	 */
+	public FunctionalException(String pMessage) {
+		super(pMessage);
+	}
 
-    // ==================== Constructeurs ====================
-    /**
-     * Constructeur.
-     *
-     * @param pMessage -
-     */
-    public FunctionalException(String pMessage) {
-        super(pMessage);
-    }
+	/**
+	 * Constructeur.
+	 * @param pCause -
+	 */
+	public FunctionalException(Throwable pCause) {
+		super(pCause);
+	}
 
-    /**
-     * Constructeur.
-     *
-     * @param pCause -
-     */
-    public FunctionalException(Throwable pCause) {
-        super(pCause);
-    }
+	/**
+	 * Constructeur.
+	 * @param pMessage -
+	 * @param pCause -
+	 */
+	public FunctionalException(String pMessage, Throwable pCause) {
+		super(pMessage, pCause);
+	}
 
-    /**
-     * Constructeur.
-     *
-     * @param pMessage -
-     * @param pCause -
-     */
-    public FunctionalException(String pMessage, Throwable pCause) {
-        super(pMessage, pCause);
-    }
 }
