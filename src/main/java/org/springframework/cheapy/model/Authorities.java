018195af627b12c600466467aab20ef51d68e74d
Javier <Javier@192.168.1.134>
2021-03-22 19:21:44 +0100

    Entidades creadas

--- src/main/java/org/springframework/cheapy/model/Authorities.java
+++ src/main/java/org/springframework/cheapy/model/Authorities.java
@@ -1,30 +1,17 @@
 package org.springframework.cheapy.model;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "authorities")
-public class Authorities extends BaseEntity {
+public class Authorities {
+	
+	@Id
+	String username;
 
-	@OneToOne
-	@JoinColumn(name = "username")
-	User user;
-
-	@Size(min = 3, max = 50)
 	String authority;
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
 
 	public String getAuthority() {
 		return authority;
