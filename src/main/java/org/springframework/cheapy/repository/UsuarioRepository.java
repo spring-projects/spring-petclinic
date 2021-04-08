ba2b2a4345b62a43d69e452b9346c9f1afa6988d
Thiloparn <48439369+Thiloparn@users.noreply.github.com>
2021-04-08 21:35:55 +0200

    Ver, modificar y borrar de Usuario funcional.

---
+++ src/main/java/org/springframework/cheapy/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package org.springframework.cheapy.repository;
+
+import java.util.List;
+
+import org.springframework.cheapy.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UsuarioRepository extends Repository<Usuario, String> {
+
+	@Query("SELECT usuario FROM Usuario usuario WHERE username =:username")
+	@Transactional(readOnly = true)
+	Usuario findByUsername(String username);
+	
+	void save(Usuario usuario);
+	
+}
