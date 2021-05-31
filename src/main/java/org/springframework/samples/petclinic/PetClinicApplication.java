5bd076b99720ff2eb2bc08e58f6ef1518a8d65ad
St ef <stef33560@gmail.com>
2021-05-31 19:10:13 +0000

    Revert "Suppression de l'annotation @SpringBootApplication(proxyBeanMethods = false)"
    This reverts commit 9888fe81db941c719f352f19fd094879ba7a5fae

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -25,6 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Dave Syer
  *
  */
+@SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
