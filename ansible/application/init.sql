c83e69118da0f81852be5bff6ac14a5c53dab4bc
Paramjeet B <36541629+paramjeet01@users.noreply.github.com>
2021-09-21 01:43:44 +0530

    Update init.sql

--- ansible/application/init.sql
+++ ansible/application/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS petclinic;
 -- GRANT ALL PRIVILEGES ON petclinic TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
-GRANT ALL PRIVILEGES ON * . * TO 'petclinic'@'%';
+GRANT ALL PRIVILEGES ON * . * TO 'petclinic'@'petclinic';
 FLUSH PRIVILEGES;
 ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
