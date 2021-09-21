73517ac8f93dc3b1c606912cc04f644b147995fb
Paramjeet B <36541629+paramjeet01@users.noreply.github.com>
2021-09-21 15:52:35 +0530

    Delete ansible directory

--- ansible/application/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
--- GRANT ALL PRIVILEGES ON petclinic TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
-GRANT ALL PRIVILEGES ON * . * TO 'petclinic'@'petclinic';
-FLUSH PRIVILEGES;
-ALTER DATABASE petclinic
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
