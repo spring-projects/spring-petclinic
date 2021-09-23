31cc443b3e47458fceed30e26918c1d8c2f9b026
drulacosmin <drulacosmin@gmail.com>
2021-09-23 05:41:24 +0000

    HIR-3 test

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * *just a comment line for testing
  */
 
 package org.springframework.samples.petclinic;
