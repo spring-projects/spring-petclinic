ac8c600d081fa93abb5161ca4461b3a9f47c0bf6
ayeliferov <86775495+ayeliferov@users.noreply.github.com>
2021-11-23 19:07:34 +0200

    var

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,6 +7,6 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = "access_key"
-  secret_key = "secret_key"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
