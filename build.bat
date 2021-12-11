2ae74f7c60f325b8c6f9c1c44e5053185066db79
Dmitriy Vorozheykin <67786455+midorun@users.noreply.github.com>
2021-12-11 19:32:47 +0400

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,5 @@
+set M2_HOME=C:\Program Files\apache-maven-3.8.4
+set path=C:\Program Files\apache-maven-3.8.4\bin;%path%;
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
+set path=C:\Program Files\Java\jdk-11.0.12\bin;%path%;
+mvn clean package
