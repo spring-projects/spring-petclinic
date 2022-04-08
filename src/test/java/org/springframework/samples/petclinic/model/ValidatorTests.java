8d3b8282efb1dd3ca66e8b02b89496a4b215df2b
MichaelKim2000 <103054850+MichaelKim2000@users.noreply.github.com>
2022-04-08 09:21:33 +0200

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -44,7 +44,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("not Empty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
