56ad66268a9786432a62e7f83cd35a6843a0e467
Jasnov <jasnovsemail@jasnov.ja>
2022-05-04 02:37:48 +0300

    commit1

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,3 +1,4 @@
+<!-- comment branch 1-->
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
