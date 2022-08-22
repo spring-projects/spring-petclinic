0ca5ff7959c796977089ee77cdb4b8dae19034b9
Ekin Devrim Şahinkaya <devrim.sahinkaya@ozu.edu.tr>
2022-08-22 18:37:05 +0300

    initial

--- src/test/java/org/springframework/samples/petclinic/test/clinicTests/SeleniumTest.java
+++ src/test/java/org/springframework/samples/petclinic/test/clinicTests/SeleniumTest.java
@@ -19,7 +19,7 @@ import org.springframework.samples.petclinic.ImpactAnalyzer.models.PageSource;
 
 import static org.junit.Assert.fail;
 
-// Initial state of the web project
+
 
 public class SeleniumTest {
 
