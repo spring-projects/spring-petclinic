0efa6c794c47e0b0593bbf426d982b5336172fa3
Dillon Sellars <dsellars@newrelic.com>
2022-09-05 16:16:42 -0400

    Fast shutdown by having entrypoint.sh listen for TERM signals.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+APP_PID=
+TESTER_PID=
+
+cleanup() {
+  echo "Killing app pid $APP_PID"
+  kill "$APP_PID"
+  wait "$APP_PID"
+  echo "Killing tester pid $TESTER_PID"
+  kill "$TESTER_PID"
+}
+
+trap cleanup INT TERM
+
 # start app
 nohup java -javaagent:/app/newrelic/newrelic.jar -jar spring-petclinic-2.7.3.jar > /app/logs/app.log 2>&1 &
+APP_PID=$!
 
 # generate load
-./tester.sh
+./tester.sh &
+TESTER_PID=$!
+
+echo "Running with APP_PID $APP_PID, TESTER_PID, $TESTER_PID"
+
+wait $TESTER_PID
