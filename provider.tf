d6ed09c57e41963da71dd9d2f30fe7497e9cef86
Qtalha <113745023+Qtalha@users.noreply.github.com>
2022-10-15 15:49:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,9 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "tlaherelkkf"  {
-    bucket = "rermdfjoju"
+resource "aws_s3_bucket" "tlaherelkkf" {
+
+        bucket = "rermdfjoju"
+
+        force_destroy = true
 }
