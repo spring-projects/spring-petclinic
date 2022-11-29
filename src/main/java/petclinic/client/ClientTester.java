0db5f7960375da5df09110b5ee7f3d51576d806a
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2022-11-29 12:48:28 +0200

    Feature/add client tester (#5)

---
+++ src/main/java/petclinic/client/ClientTester.java
@@ -0,0 +1,111 @@
+package petclinic.client;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class ClientTester {
+
+	private static final String BASE_APP_URL = "http://localhost:9876";
+
+	public static void main(String[] args) {
+		MyClient myClient = new MyClient(BASE_APP_URL);
+
+		for (int ix = 1; ix <= 5; ix++) {
+			myClient.execute("/vets.html");
+		}
+		for (int ix = 1; ix <= 2; ix++) {
+			myClient.execute("/oups", false);
+		}
+		for (int ix = 1; ix <= 3; ix++) {
+			myClient.execute("/appErr1", false);
+		}
+		for (int ix = 1; ix <= 4; ix++) {
+			myClient.execute("/appErr2", false);
+		}
+
+		for (int ix = 1; ix <= 2; ix++) {
+			myClient.execute("/");
+		}
+
+		for (int ix = 1; ix <= 7; ix++) {
+			myClient.execute("/owners/find");
+		}
+		// few of those might return error, since not all ids exists
+		for (int ownerId = 1; ownerId <= 20; ownerId++) {
+			myClient.execute("/owners/" + ownerId, false);
+		}
+		for (int ix = 1; ix <= 6; ix++) {
+			myClient.execute("/owners?lastName=Davis");
+		}
+		for (int ix = 1; ix <= 6; ix++) {
+			myClient.execute("/owners?lastName=Spring");
+		}
+		// using existing owners
+		for (int ownerId = 2; ownerId <= 6; ownerId++) {
+			// calling edit (getting the form)
+			myClient.execute(String.format("owners/%d/edit", ownerId));
+			// calling add new pet (getting the form)
+			myClient.execute(String.format("owners/%d/pets/new", ownerId));
+		}
+
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				System.out.println("Shutting down...");
+				Thread.sleep(567);
+			} catch (InterruptedException e) {
+				Thread.interrupted();
+			}
+		}));
+	}
+
+	static class MyClient {
+
+		private final OkHttpClient client;
+		private final String baseUrl;
+
+		private MyClient(String baseUrl) {
+			this.baseUrl = baseUrl;
+
+			this.client = new OkHttpClient.Builder()
+				.build();
+		}
+
+		public void execute(String uriPath, boolean throwExceptionWhenCodeIsNot200) {
+			Request request = new Request.Builder()
+				.url(baseUrl + assureStartsWithSlash(uriPath))
+				.build();
+
+			Call call = client.newCall(request);
+			try (Response response = call.execute()) {
+				if (throwExceptionWhenCodeIsNot200) {
+					int code = response.code();
+					if (code != 200) {
+						throw new RuntimeException(String.format("URI '%s' returned code %d", uriPath, code));
+					}
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		public void execute(String uriPath) {
+			execute(uriPath, true);
+		}
+
+		private static String assureStartsWithSlash(String relativeUri) {
+			if (relativeUri == null) {
+				return "/";
+			}
+			String trimmedUri = relativeUri.trim();
+			if (trimmedUri.startsWith("/")) {
+				return relativeUri;
+			}
+			return '/' + trimmedUri;
+		}
+	}
+}
