cc4667122d4f06946f809389762a1a39b3dd1a41
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2023-01-02 22:54:46 +0100

    Resource bundle not included in native image

--- src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
@@ -24,6 +24,8 @@ public class PetClinicRuntimeHints implements RuntimeHintsRegistrar {
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
 		hints.resources().registerPattern("db/*"); // https://github.com/spring-projects/spring-boot/issues/32654
+		hints.resources().registerPattern("messages/*");
+		hints.resources().registerPattern("META-INF/resources/webjars/*");
 	}
 
 }
