ab502c267203b038b188b47bdb0f10d4ddbae967
jstan-isch <52019536+jstan-isch@users.noreply.github.com>
2023-01-28 15:58:21 -0600

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -8,4 +8,4 @@
           -H "Accept: application/vnd.github+json" \
           -H "Authorization: Bearer $GITHUB_TOKEN" \
           https://api.github.com/repos/jstan-isch/spring-petclinic/check-runs \
-          -d '{"name":"$NAME", "head_sha":"$SHA", "status":"completed", "conclusion":"$STATUS"}'
+          -d '{"name":"$NAME", "head_sha":"$SHA", "status":"completed", "conclusion":"success"}'
