b4a2a16aec71c46e9f12ab4ef03c360934d487fe
jstan-isch <52019536+jstan-isch@users.noreply.github.com>
2023-01-28 16:32:11 -0600

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -3,19 +3,19 @@
 # Github app uses this script to create check runs
 # for a commit sha
 
-echo PR_CONTEXT="$CONTEXT"
-echo COMMIT_SHA="$SHA"
-echo PR_STATUS="$STATUS"
-echo TOKEN="$GITHUB_TOKEN"
+# echo PR_CONTEXT="$CONTEXT"
+# echo COMMIT_SHA="$SHA"
+# echo PR_STATUS="$STATUS"
+# echo TOKEN="$GITHUB_TOKEN"
 
-PR_CONTEXT="$CONTEXT"
-COMMIT_SHA="$SHA"
-PR_STATUS="$STATUS"
-TOKEN="$GITHUB_TOKEN"
+# PR_CONTEXT="$CONTEXT"
+# COMMIT_SHA="$SHA"
+# PR_STATUS="$STATUS"
+# TOKEN="$GITHUB_TOKEN"
 
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${TOKEN}" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   https://api.github.com/repos/jstan-isch/spring-petclinic/check-runs \
- -d '{"name":"$PR_CONTEXT, "head_sha":$COMMIT_SHA, "status":"completed", "conclusion":$PR_STATUS}'
+ -d '{"name":"$CONTEXT", "head_sha": "$SHA", "status":"completed", "conclusion": "$STATUS"}'
