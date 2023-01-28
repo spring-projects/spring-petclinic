68c82dbc51a4595bbe8a0c5d60502c0c93ef8d67
jstan-isch <52019536+jstan-isch@users.noreply.github.com>
2023-01-28 16:02:19 -0600

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -3,6 +3,11 @@
 # Github app uses this script to create check runs
 # for a commit sha
 
+echo NAME="myEvent"
+echo STATUS="success"
+echo SHA="$SHA"
+echo GITHUB_TOKEN="$GITHUB_TOKEN"
+
 /usr/bin/curl \
           -X POST \
           -H "Accept: application/vnd.github+json" \
