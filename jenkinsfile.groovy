916e3baa66c2f9683dd24cbe92b6f7aa7afa6644
HOLYvier <63263121+HOLYvier-Buh@users.noreply.github.com>
2023-03-03 11:39:20 +0100

    Create jenkinsfile.groovy
    for jenkins pipeline

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn compile'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Package') {
+            steps {
+                sh 'mvn package'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'mvn deploy'
+            }
+        }
+    }
+}
