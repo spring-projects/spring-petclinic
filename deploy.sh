cbc504bb604c45fbe5fcd4828a42f406ceed6c98
GasparAM <gaspar1999@live.com>
2023-04-09 18:54:54 +0400

    fixed awk

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 
-PVER=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout | awk 'BEGIN {FS="-"} ; {print $2}')
+PVER=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout | awk 'BEGIN {FS="-"} ; {print $1}')
 DVER=$(echo ${PVER} | awk 'BEGIN {FS="."}; {print $1"."$2"."$3 + 1}')
 
-./mvnw -B release:prepare -Dtag=$PVER -DreleaseVersion=$PVER -DdevelopmentVersion=$DVER &&
+./mvnw -B release:prepare -Dtag=$PVER -DreleaseVersion=$PVER -DdevelopmentVersion=$DVER-SNAPSHOT &&
 ./mvnw release:perform -Darguments="-Dmaven.deploy.skip=true"
