9868d3e7f070238f53c6bf779da6e1babc4f70c8
GasparAM <gaspar1999@live.com>
2023-04-09 18:58:18 +0400

    potential fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 PVER=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout | awk 'BEGIN {FS="-"} ; {print $1}')
 DVER=$(echo ${PVER} | awk 'BEGIN {FS="."}; {print $1"."$2"."$3 + 1}')
 
-./mvnw -B release:prepare -Dtag=$PVER -DreleaseVersion=$PVER -DdevelopmentVersion=$DVER-SNAPSHOT &&
+./mvnw -B release:prepare -Dtag=$PVER -DreleaseVersion=$PVER -DdevelopmentVersion="${DVER}-SNAPSHOT" &&
 ./mvnw release:perform -Darguments="-Dmaven.deploy.skip=true"
