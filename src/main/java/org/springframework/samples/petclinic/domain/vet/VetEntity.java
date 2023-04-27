07c3e10bfbd20baf6f7262d948dc68f4e56a865a
Ismail Ismailov <ismail2ov@gmail.com>
2023-04-27 14:53:25 +0200

    Create domain objects

---
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/VetEntity.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.domain.vet;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.samples.petclinic.domain.model.PersonModel;
+import org.springframework.samples.petclinic.infrastructure.persistence.model.PersonEntity;
+
+public class VetEntity extends PersonModel {
+
+	private Set<Specialty> specialties;
+
+	protected Set<Specialty> getSpecialtiesInternal() {
+		if (this.specialties == null) {
+			this.specialties = new HashSet<>();
+		}
+		return this.specialties;
+	}
+
+	protected void setSpecialtiesInternal(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
+
+	@XmlElement
+	public List<Specialty> getSpecialties() {
+		return new ArrayList<>(getSpecialtiesInternal());
+	}
+
+	public int getNrOfSpecialties() {
+		return getSpecialtiesInternal().size();
+	}
+
+	public void addSpecialty(Specialty specialty) {
+		getSpecialtiesInternal().add(specialty);
+	}
+
+}
