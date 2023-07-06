26a63182693ae4f8bfc33c1a890edeef6374b9a9
s4616 <87678241+s4616@users.noreply.github.com>
2023-07-06 15:23:42 +0900

    Delete jenkins-script directory

--- jenkins-script/install-docker-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-## INFO: https://docs.docker.com/compose/install/
-
-set -euf pipefail
-DOCKER_COMPOSE_VERSION=v2.1.1
-
-# Download and install
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
--- jenkins-script/scripts/install-awscli.sh
+++
@@ -1,4 +0,0 @@
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip -y
-unzip awscliv2.zip
-sudo ./aws/install
