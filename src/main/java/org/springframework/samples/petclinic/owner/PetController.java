86fa0ee8e022617429dee50a3e730f4bcc796fba
bijomutta <bijomutta@gmail.com>
2023-07-14 11:06:54 +0200

    running java-format

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -57,7 +57,6 @@ class PetController {
 	@ModelAttribute("owner")
 	public Owner findOwner(@PathVariable("ownerId") int ownerId) {
 
-
 		Owner owner = this.owners.findById(ownerId);
 		if (owner == null) {
 			throw new IllegalArgumentException("Owner ID not found: " + ownerId);
