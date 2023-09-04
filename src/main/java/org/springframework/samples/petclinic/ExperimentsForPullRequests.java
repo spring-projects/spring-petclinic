84b7c5d0a402a72e5285ddfd94cd989e855d457f
anton <ant.arhipov@gmail.com>
2023-09-04 13:19:28 +0300

    potential NPE

---
+++ src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.lang.Nullable;
+
+public class ExperimentsForPullRequests {
+
+	public static void foo(@Nullable String s){
+		System.out.println("String length: " + s.length());
+	}
+
+}
