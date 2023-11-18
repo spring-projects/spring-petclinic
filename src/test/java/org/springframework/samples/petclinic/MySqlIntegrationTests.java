dedb25c9b9724f0b06fe90bf47627191384a4171
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2023-11-18 16:55:56 +0100

    Updating MySql to 8.2

--- src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
@@ -44,7 +44,7 @@ class MySqlIntegrationTests {
 
 	@ServiceConnection
 	@Container
-	static MySQLContainer<?> container = new MySQLContainer<>("mysql:5.7");
+	static MySQLContainer<?> container = new MySQLContainer<>("mysql:8.2");
 
 	@LocalServerPort
 	int port;
--- src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
+++ src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
@@ -36,7 +36,7 @@ public class MysqlTestApplication {
 	@Profile("mysql")
 	@Bean
 	static MySQLContainer<?> container() {
-		return new MySQLContainer<>("mysql:8.1");
+		return new MySQLContainer<>("mysql:8.2");
 	}
 
 	public static void main(String[] args) {
