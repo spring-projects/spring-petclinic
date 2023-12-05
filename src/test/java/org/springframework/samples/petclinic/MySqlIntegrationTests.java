089135a56c24bc9dc491e470715c775187c7d58e
Rob Oxspring <rob.oxspring@diffblue.com>
2023-12-05 20:02:25 +0000

    Running `./gradlew test` should not require Docker

--- src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
@@ -31,6 +31,7 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.vet.VetRepository;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -40,6 +41,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ActiveProfiles("mysql")
 @Testcontainers(disabledWithoutDocker = true)
 @DisabledInNativeImage
+@DisabledInAotMode
 class MySqlIntegrationTests {
 
 	@ServiceConnection
