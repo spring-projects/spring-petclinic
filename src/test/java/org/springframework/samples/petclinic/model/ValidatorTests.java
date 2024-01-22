fd7caf4f7249868093c8682076c3c207fd8028e2
P-Kalyan426 <p.kalyan8888@gmail.com>
2024-01-22 23:11:51 +0530

    Revert "break a test"
    This reverts commit 299f02e09b96f935525967f22acc72b15b011d80.
    
    wq

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("notempty");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
