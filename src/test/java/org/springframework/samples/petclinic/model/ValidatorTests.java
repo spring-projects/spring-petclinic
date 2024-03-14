57eb7443a638493d1c81672a29fb712eb1b99b4a
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-03-14 09:23:01 +0100

    test: modify assertion for increasing readability.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -53,7 +53,7 @@ class ValidatorTests {
 
 		assertThat(constraintViolations).hasSize(1);
 		ConstraintViolation<Person> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+		assertThat(violation.getPropertyPath().toString()).hasToString("firstName");
 		assertThat(violation.getMessage()).isEqualTo("must not be blank");
 	}
 
