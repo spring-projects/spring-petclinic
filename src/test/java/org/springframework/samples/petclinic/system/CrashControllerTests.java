634cc35cf4d846d391bf4eb37176618803a27b69
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-03-17 15:50:14 +0100

    chore: optimize imports

--- src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
@@ -16,11 +16,9 @@
 
 package org.springframework.samples.petclinic.system;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 /**
  * Test class for {@link CrashController}
