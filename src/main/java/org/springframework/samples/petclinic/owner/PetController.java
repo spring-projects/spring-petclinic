1b923650e28ec206cff97677956ae206570e6664
dangerbaldo <daniele.gerbaldo@gmail.com>
2024-03-27 21:48:13 +0100

    variable restored

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -44,8 +44,6 @@ class PetController {
 
 	private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
 	public String tstvar = null;
-	public String unused = null; //adding an unused variable...
-	
 	
 	private final OwnerRepository owners;
 
