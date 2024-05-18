92b520fdf3bffd9042a21f5002c6bb95e31b936e
Tommaso Gionfriddo <tommaso@di.net.au>
2024-05-19 00:39:23 +1000

    Failed test

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -17,6 +17,7 @@
 package org.springframework.samples.petclinic;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,6 +48,12 @@ public class PetClinicIntegrationTests {
 	void testFindAll() throws Exception {
 		vets.findAll();
 		vets.findAll(); // served from cache
+	}
+
+
+	@Test
+	void testDemo(){
+		assertEquals(1, 2);
 	}
 
 	@Test
