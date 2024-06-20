1fdd61ef675d4afca118b85213db812281d8159d
Barry Matheney <bmatheney@clearlyagile.com>
2024-06-20 15:29:43 -0400

    20240620_152943

--- scr_non_secret_env_vars.sh
+++ scr_non_secret_env_vars.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 echo "$(date +%Y%m%d_%H%M%S) - Entering $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
 
+export DOCKER_IMAGE_NAME="${BITBUCKET_REPO_FULL_NAME_SCRUBBED}"
+export DOCKER_TAG_COMMIT_SHA="${BITBUCKET_COMMIT_SCRUBBED}"
+export DOCKER_TAG_COMMIT_SHA="${BITBUCKET_COMMIT_SCRUBBED}"
+export DOCKER_TAG_BRANCH_NAME="${BITBUCKET_BRANCH_SCRUBBED}"
+export DOCKER_TAG_BRANCH_NAME="${BITBUCKET_PIPELINE_UUID_SCRUBBED}"
 echo "$(date +%Y%m%d_%H%M%S) - Leaving $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
 
--- scr_scrub_bitbucket_env_vars.sh
+++ scr_scrub_bitbucket_env_vars.sh
@@ -1,32 +1,46 @@
 #!/usr/bin/env bash
+echo "$(date +%Y%m%d_%H%M%S) - Entering $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
+
+if [ -z ${BITBUCKET_BRANCH+x} ]; then #  this shouldn't be defined locally, so we can use it to determine local vs pipeline run and tests...
+  source scr_test_pipeline_variables.sh
+else
+  echo "Running in a pipeline, using Bitbuckets real variable values."
+fi
+
 # List of characters to replace
 declare -A char_replacements=(
-    [" "]=_
-    ["/"]=_
-    [":"]=_
-    ["("]=_
-    [")"]=_
-    ["{"]=_
-    ["}"]=_
-    ["-"]=_
+  [" "]=_
+  ["/"]=_
+  [":"]=_
+  ["("]=_
+  [")"]=_
+  ["{"]=_
+  ["}"]=_
+  ["-"]=_
 )
 
-# Function to scrub a string
 scrub_string() {
-    local string=$1
-    for char in "${!char_replacements[@]}"; do
-        string=${string//"$char"/"${char_replacements[$char]}"}
-    done
-    echo "$string"
+  local string=$1
+  # Replace characters
+  for char in "${!char_replacements[@]}"; do
+    string=${string//"$char"/"${char_replacements[$char]}"}
+  done
+  # Replace multiple underscores with a single underscore
+  string=$(echo "$string" | sed 's/[_]\{2,\}/_/g')
+  # Remove leading and trailing underscores
+  string=$(echo "$string" | sed 's/^_//;s/_$//')
+  echo "$string"
 }
 
 # Iterate through environment variables
 for var in $(printenv | grep '^BITBUCKET_' | cut -d= -f1); do
-    # Get the value of the variable
-    value=$(printenv "$var")
-    # Scrub the value
-    scrubbed_value=$(scrub_string "$value")
-    # Create a new variable with the scrubbed value
-    export "${var}_SCRUBBED=$scrubbed_value"
-    echo "${var}_SCRUBBED=$scrubbed_value"
+  # Get the value of the variable
+  value=$(printenv "$var")
+  # Scrub the value
+  scrubbed_value=$(scrub_string "$value")
+  # Create a new variable with the scrubbed value
+  export "${var}_SCRUBBED=$scrubbed_value"
+  echo "${var}_SCRUBBED=$scrubbed_value"
 done
+
+echo "$(date +%Y%m%d_%H%M%S) - Leaving $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
