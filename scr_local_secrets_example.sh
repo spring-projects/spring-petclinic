096099ae71fb22ec367b0df05f49b5d69d8c56e7
Barry Matheney <bmatheney@clearlyagile.com>
2024-06-25 13:17:55 -0400

    20240625_131755

--- scr_local_secrets_example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "$(date +%Y%m%d_%H%M%S) - Entering $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
-
-export HARBOR_USERNAME="barry_matheney@yahoo.com"
-export HARBOR_LOGIN_ADDRESS="c8n.io/barrymatheney"
-export HARBOR_ACCESS_TOKEN="whatever the access token that you created is"
-
-echo "$(date +%Y%m%d_%H%M%S) - Leaving $(basename "$(realpath "${BASH_SOURCE[0]}")") on host $(hostname)"
-
