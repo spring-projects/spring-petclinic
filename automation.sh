a94f441f44a845b69647247ecf342c480e592a34
Nicholas Mucks <njmuck@dolby.net>
2024-07-14 12:27:36 -0700

    update docker and add an automation.sh

---
+++ automation.sh
@@ -0,0 +1,14 @@
+echo "####################################################"
+echo "Stop and remove all running containers..."
+echo "docker stop $(docker ps -a -q)"
+echo "docker system prune -af"
+docker stop $(docker ps -a -q)
+docker system prune -af
+
+echo "####################################################"
+echo "Build and run container..."
+echo "build . -f spring_image"
+echo "docker run -itd --net=assignment2 -p:8080:8080 --name spring-clinic spring_image"
+docker build . -f spring_image
+docker run -itd --net=assignment2 -p:8080:8080 --name spring-clinic spring_image
+
