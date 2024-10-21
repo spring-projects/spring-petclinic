08dc8c020a476d4e9e7451a5bd2e22a7d41ebc21
Eduard Kuznetsov <fin3dom@gmail.com>
2024-10-21 13:55:18 +0300

    Deleted all tests

---
+++ src/test/java/org/springframework/samples/petclinic/EmptyTest.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test that created only for successful jacoco report generation in case if there are no
+ * tests.
+ */
+class EmptyTest {
+
+	@Test
+	void emptyTest() {
+	}
+
+}
