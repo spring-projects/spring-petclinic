91f55a4f7134409a832435de2c6df231102d5898
Dave Syer <dsyer@vmware.com>
2024-11-27 08:32:30 +0000

    Versionless webjars (again)
    Spring Boot 3.4 supports the webjars-locator-lite which in turn
    supports native images, so we are back to versionless URLs for
    webjars assets in templates.

--- src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
@@ -28,7 +28,6 @@ public class PetClinicRuntimeHints implements RuntimeHintsRegistrar {
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
 		hints.resources().registerPattern("db/*"); // https://github.com/spring-projects/spring-boot/issues/32654
 		hints.resources().registerPattern("messages/*");
-		hints.resources().registerPattern("META-INF/resources/webjars/*");
 		hints.resources().registerPattern("mysql-default-conf");
 		hints.serialization().registerType(BaseEntity.class);
 		hints.serialization().registerType(Person.class);
