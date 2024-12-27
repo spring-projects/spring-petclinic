1aa1311497facdb89a59f9fa0332ac709cdedb65
abdelrahmanassem <abdoazwak55@gmail.com>
2024-12-27 13:23:51 +0200

    Refactor remove uneccasry  comments

--- src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
@@ -37,7 +37,7 @@ public class PetValidator implements Validator {
 	public void validate(Object obj, Errors errors) {
 		Pet pet = (Pet) obj;
 		String name = pet.getName();
-		// name validation
+
 		if (!StringUtils.hasText(name)) {
 			errors.rejectValue("name", REQUIRED, REQUIRED);
 		}
