962375a7fe1eda36a3b002476539b2ab34baab7a
jeet041 <mahorjitendra@gmail.com>
2025-04-24 01:11:38 +0530

    Update UserService.java
    Signed-off-by: jeet041 <mahorjitendra@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/UserService.java
+++ src/main/java/org/springframework/samples/petclinic/UserService.java
@@ -1,28 +1,21 @@
 public class UserService {
     public String getUserName(int userId) {
-        return "[Main] User#" + userId;
+        return "User" + userId;
     }
 
     public boolean isUserActive(int userId) {
-        System.out.println("Main branch: checking DB for user activity");
         return true;
     }
 
     public void updateUserStatus(int userId, boolean isActive) {
-        System.out.println("Main branch: logging status update");
-        // update logic to DB
+        // Update user status in DB
+    }
+
+    public void notifyUser(int userId) {
+        // Send notification
     }
 
     public void deleteUser(int userId) {
-        logDeletion(userId);
-        // deletion logic
-    }
-
-    public void notifyUser(int userId, String message) {
-        System.out.println("[Main] Sending message to user: " + message);
-    }
-
-    private void logDeletion(int userId) {
-        System.out.println("Logging deletion of user " + userId);
+        // Delete user logic
     }
 }
