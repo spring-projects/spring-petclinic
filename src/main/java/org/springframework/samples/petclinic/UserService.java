e63769936d121a601be4668cc00af523e1c0b0c9
jeet041 <mahorjitendra@gmail.com>
2025-04-24 01:17:58 +0530

    Update UserService.java
    Signed-off-by: jeet041 <mahorjitendra@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/UserService.java
+++ src/main/java/org/springframework/samples/petclinic/UserService.java
@@ -4,15 +4,16 @@ public class UserService {
     }
 
     public boolean isUserActive(int userId) {
-        return false;
+        System.out.println("Checking DB for active user");
+        return true;
     }
 
     public void updateUserStatus(int userId, boolean isActive) {
-        System.out.println("Feature branch: updating status to " + isActive);
+        System.out.println("Main branch: status set to " + isActive);
     }
 
     public void notifyUser(int userId) {
-        System.out.println("Feature branch: notifying user " + userId);
+        System.out.println("Main: sending notification to user " + userId);
     }
 
     public void deleteUser(int userId) {
