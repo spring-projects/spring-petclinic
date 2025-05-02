266a31c081c17afc344f6d0824600a914a48e3b5
jeet041 <mahorjitendra@gmail.com>
2025-05-02 17:40:52 +0530

    Update test_conflict.java
    Signed-off-by: jeet041 <mahorjitendra@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/test_conflict.java
+++ src/main/java/org/springframework/samples/petclinic/test_conflict.java
@@ -4,7 +4,6 @@ public class HelloWorld {
     }
 
     public static void helloWorld() {
-        System.out.println("Hello, World! main");
         System.out.println("Hello, World! dev");
     }
 }
