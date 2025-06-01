08e70f65030854ea424121f8984f92a60282a9a7
rahulgauli <63265876+rahulgauli@users.noreply.github.com>
2025-06-01 00:13:42 -0500

    Update PetClinicApplication.java
    Signed-off-by: rahulgauli <63265876+rahulgauli@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -7,7 +7,7 @@
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
  * yei ko full huncha ki hunaa? 
- 
+ * yei ho?
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
