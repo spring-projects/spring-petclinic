ead52612b55ae6215146f1458c11e52a996cffe6
Abdul Moiz Bin Tahir <abdul.moiz@easypaisa.com.pk>
2025-08-06 10:49:05 +0500

    Add dummy failing test to verify CI failure
    Signed-off-by: Abdul Moiz Bin Tahir <abdul.moiz@easypaisa.com.pk>

---
+++ src/test/java/org/springframework/samples/petclinic/DummyTest.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DummyTest {
+
+	@Test
+	void thisTestWillAlwaysFail() {
+		fail("This is a dummy failing test");
+	}
+}
