3e1374e5e378c8b136056d395a2739f383a5dcf1
Abdul Moiz Bin Tahir <abdul.moiz@easypaisa.com.pk>
2025-08-06 11:16:59 +0500

    Comment out dummy failing test to fix CI
    Signed-off-by: Abdul Moiz Bin Tahir <abdul.moiz@easypaisa.com.pk>

--- src/test/java/org/springframework/samples/petclinic/DummyTest.java
+++ src/test/java/org/springframework/samples/petclinic/DummyTest.java
@@ -3,11 +3,11 @@ package org.springframework.samples.petclinic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
-
+/*
 public class DummyTest {
 
 	@Test
 	void thisTestWillAlwaysFail() {
 		fail("This is a dummy failing test");
 	}
-}
+}*/
