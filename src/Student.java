4f880d92aae26d6af2709f6480cb1f46fd39d755
Adit-Mugdha-Das <mailbox.mugdha@gmail.com>
2025-09-20 09:18:53 +0600

    conflict

--- src/Student.java
+++ src/Student.java
@@ -10,6 +10,6 @@ public class Student {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public int getId() { return id; }
+   
     public void setId(int id) { this.id = id; }
 }
