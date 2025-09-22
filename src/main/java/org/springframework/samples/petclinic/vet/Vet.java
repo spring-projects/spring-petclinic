0a4ead62e18558ef92253c441bf00ad34aca96be
pranjalwagh <pranjalwagh612001@gmail.com>
2025-09-23 01:05:19 +0530

    test cicd 11

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -59,7 +59,6 @@ public class Vet extends Person {
 	@XmlElement
 	public List<Specialty> getSpecialties() {
 		// This is safe because we return a copy
-		//  and because Specialty is immutable
 		return getSpecialtiesInternal().stream()
 			.sorted(Comparator.comparing(NamedEntity::getName))
 			.collect(Collectors.toList());
