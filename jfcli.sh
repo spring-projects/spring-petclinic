c662ba12edd69f2250a05d2e5a0d1f64269b9f76
Krishna Manchikalapudi <krishnam@jfrog.com>
2025-10-01 14:02:36 -0400

    jenkins file

--- jfcli.sh
+++ jfcli.sh
@@ -1,9 +1,11 @@
 
 export JF_NAME="psazuse" JFROG_CLI_LOG_LEVEL="DEBUG" 
-export JF_RT_URL="https://${JF_NAME}.jfrog.io" RT_REPO_VIRTUAL="krishnam-mvn-virtual" 
+export JF_RT_URL="https://${JF_NAME}.jfrog.io" RT_REPO_VIRTUAL="springpetclinic-mvn-virtual" 
 
 export BUILD_NAME="spring-petclinic" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" 
 
 jf mvnc --global --repo-resolve-releases ${RT_REPO_VIRTUAL} --repo-resolve-snapshots ${RT_REPO_VIRTUAL} 
+
+jf ca --format=table --threads=100
 
 jf mvn clean install --build-name=${BUILD_NAME} --build-number=${BUILD_ID} 
