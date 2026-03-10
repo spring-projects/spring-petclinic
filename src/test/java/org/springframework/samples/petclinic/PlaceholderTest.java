49c47f9d171d8866d19c601dce32245816868a7f
Akseben <alijagic22@outlook.com>
2026-03-10 13:54:54 -0500

    Initial commit: adding jacoco and a placeholder test to spring petclinic

---
+++ src/test/java/org/springframework/samples/petclinic/PlaceholderTest.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlaceholderTest {
+
+	@Test
+	public void testPlaceholder() {
+		assertTrue(true);
+	}
+
+}
